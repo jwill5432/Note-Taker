4c77a9961bce77b80802826d7f1a22e22c021b79
jwill5432 <110855104+jwill5432@users.noreply.github.com>
2023-02-11 21:20:42 -0500

    <finishing up>

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-/*const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/develop/public/index.html');
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/develop/public/notes.html');
-});
-
-
-app.post('/notes', (req, res) => {
-  fs.readFile(__dirname + '/develop/db/db.json', (err, data) => {
-    if (err) throw err;
-    let notes = JSON.parse(data);
-    notes.push(req.body);
-    fs.writeFile(__dirname + '/db.json', JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.send('Note added successfully!');
-    });
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Note taker app is listening on port ${port}!`);
-});*/
-
 const { prototype } = require("events");
 const express = require("express");
 const app = express();
